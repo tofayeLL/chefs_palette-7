088ab2cec71b6de549258538520c9defd1ca1c45
tofayeLL <ahmedtofayel132@gmail.com>
2024-03-15 22:47:25 +0600

    added initial files

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+import './App.css'
+
+function App() {
+ 
+  return (
+    <>
+     
+      <h1>Vite + React</h1>
+      <div className='grid grid-cols-4 gap-4'>
+      <div>  <img src="https://i.ibb.co/P9zdXfx/grilledsalmon.jpg" alt="" /></div>
+      <div>  <img src="https://i.ibb.co/kDTSWJ5/caesar-salad.jpg" alt="" /></div>
+      <div>  <img src="https://i.ibb.co/GC80xpw/vegetable-stir-fry.jpg" alt="" /></div>
+      </div>
+     
+      
+    </>
+  )
+}
+
+export default App
